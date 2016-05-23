8854454c37ee065732c5a090313da908ee88d60e
Ming1908 <wangminglu_1908@163.com>
2016-05-23 23:23:24 +0800

    新增加一个应用polls,并且编写了polls/models.py,新增了Question和Choice

---
+++ polls/models.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+#! -*- coding:utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    
+    question_text = models.CharField(max_length=200)
+    
+    pub_data = models.DateTimeField('data published')
+
+
+class Choice(models.Model):
+
+    question = models.ForeignKey(Question)
+
+    choice_text = models.CharField(max_length=200)
+
+    votes = models.IntegerField(default=0)
+
+
