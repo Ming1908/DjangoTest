2f616d1da4fef4ede09c13b5e0bb78cfe314aa47
Ming1908 <wangminglu_1908@163.com>
2016-05-23 23:31:49 +0800

    修改models.py 的pub_dateå变量名

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,7 @@ class Question(models.Model):
     
     question_text = models.CharField(max_length=200)
     
-    pub_data = models.DateTimeField('data published')
+    pub_date = models.DateTimeField('data published')
 
 
 class Choice(models.Model):
